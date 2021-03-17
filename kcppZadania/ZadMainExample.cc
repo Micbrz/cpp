96271e0a27fe92326804597d129ca7f492850fa6
Micbrz <65845479+Micbrz@users.noreply.github.com>
2021-03-17 10:03:37 +0100

    Add files via upload

---
+++ kcppZadania/ZadMainExample.cc
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+extern "C" void abc(int);
+
+extern "C"
+{
+    int a;
+    
+}
+
+void dodawanie()
+{
+    cout << "dodawanie" << endl;
+}
+
+void odejmowanie()
+{
+    cout << "odejmowanie" << endl;
+}
+
+
+
+void mnozenie()
+{
+    cout << "mnozenie" << endl;
+}
+
+int liczba;
+int main()
+{
+    cout << "Podaj od 1 do 5: " ;
+    cin >> liczba; 
+
+    if (liczba == 1)
+    {
+        return dodawanie();
+    }
+    else if (liczba == 2)
+    {
+        return odejmowanie();
+
+    }
+    else if (liczba == 3)
+    {
+        return mnozenie();
+    }
+}
+
