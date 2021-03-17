13713d2068e67e2eeeb0b8f064e8aa34d858eded
Micbrz <65845479+Micbrz@users.noreply.github.com>
2021-03-17 13:55:29 +0100

    Add files via upload

---
+++ kcppZadania/ZadZwracanie.cc
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+int &ref()
+{
+    int num = 8;
+    return num;
+}
+
+int* wskaznik()
+{
+    int a = 2;
+    int* b = &a;
+    return &a;
+}
+
+int value()
+{
+    return 7;
+}
+
+int* tab()
+{
+    int tablica[4]{ 1,2,3,4 };
+    return tablica;
+}
+
+int main()
+{
+    int a, *b;
+    int* arr;
+    a = ref();
+    b = wskaznik();
+    cout << "\n" << *b;
+    a = value();
+    cout << "\n" << a << endl;
+    arr = tab();
+    for (int i = 0; i < 3; i++)
+    {
+        cout << *(arr + i);
+    }
+    
+}
