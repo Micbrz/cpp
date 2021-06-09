541cf51d4588095a297c68bf9c5142c52c83b475
Micbrz <65845479+Micbrz@users.noreply.github.com>
2021-06-09 12:05:26 +0200

    Add files via upload

---
+++ kcppZadania/ZadUnia.cc
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+union Unia{
+    int first;
+    float second;
+};
+
+
+int main()
+{
+
+    Unia a, b;
+
+    a.first = 1;
+    a.second = 2;
+    cout << a.first << " " << a.second << endl;
+
+    return 0;
+}
